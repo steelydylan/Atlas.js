924c982abdc040d79dbce140119b696db2d9e7b8
hori godai <ess_president@me.com>
2013-04-19 13:29:32 +0900

    commit

---
+++ gh-pages/document.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $("#content").load("./html/content1.html");
+    $(".sidemenu").click(function () {
+        $(".sidemenu").each(function () {
+            $(this).parent("li").removeClass("active");
+        });
+        $(this).parent("li").addClass("active");
+        var data = "./html/";
+        data += $(this).attr("href").slice(1);
+        console.log(data);
+        $("#content").load(data);
+    });
+});
