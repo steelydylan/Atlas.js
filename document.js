80c70e4bb2903ec0de3418697f8f5c45979f328b
hori godai <ess_president@me.com>
2013-04-20 18:55:20 +0900

    Game Engine

--- document.js
+++ document.js
@@ -1,5 +1,5 @@
 ﻿$(document).ready(function () {
-    $("#content").load("./html/content1.html");
+    $("#content").load("./html/content1.html", function () { prettyPrint(); });
     $(".sidemenu").click(function () {
         $(".sidemenu").each(function () {
             $(this).parent("li").removeClass("active");
@@ -8,6 +8,6 @@
         var data = "./html/";
         data += $(this).attr("href").slice(1);
         console.log(data);
-        $("#content").load(data);
+        $("#content").load(data, function () { prettyPrint(); });
     });
 });
