62605e60478a9b4f00cc8b552c1575a5fbe435f3
hori godai <ess_president@me.com>
2013-05-27 22:18:53 +0900

    game engine

--- document.js
+++ document.js
@@ -1,13 +1,28 @@
-﻿$(document).ready(function () {
-    $("#content").load("./html/content1.html", function () { prettyPrint(); });
-    $(".sidemenu").click(function () {
-        $(".sidemenu").each(function () {
-            $(this).parent("li").removeClass("active");
+﻿$(function () {
+    if (language && language == "English")
+        $("#content2").load("./html2/content1.html", function () { prettyPrint(); });
+    else
+        $("#content").load("./html/content1.html", function () { prettyPrint(); });
+        $(".sidemenu").click(function () {
+            if (language == "English") {
+                $(".sidemenu").each(function () {
+                    $(this).parent("li").removeClass("active");
+                });
+                $(this).parent("li").addClass("active");
+                var data = "./html2/";
+                data += $(this).attr("href").slice(1);
+                console.log(data);
+                $("#content2").load(data, function () { prettyPrint(); });
+            } else {
+                $(".sidemenu").each(function () {
+                    $(this).parent("li").removeClass("active");
+                });
+                $(this).parent("li").addClass("active");
+                var data = "./html/";
+                data += $(this).attr("href").slice(1);
+                console.log(data);
+                $("#content").load(data, function () { prettyPrint(); });
+            }
         });
-        $(this).parent("li").addClass("active");
-        var data = "./html/";
-        data += $(this).attr("href").slice(1);
-        console.log(data);
-        $("#content").load(data, function () { prettyPrint(); });
-    });
+
 });
---
+++ multilingal.js
@@ -0,0 +1,28 @@
+﻿var language;
+$(function () {
+    language = window.localStorage.getItem("language");
+    if (language && language == "English") {
+        document.getElementById("Japanese").style.display = "none";
+        document.getElementById("English").style.display = "block";
+    }
+});
+
+function Display(no) {
+
+    if (no == "Japanese") {
+
+        document.getElementById("Japanese").style.display = "block";
+        document.getElementById("English").style.display = "none";
+        window.localStorage.setItem("language", "Japanese");
+        language = "Japanese";
+
+    } else if (no == "English") {
+
+        document.getElementById("Japanese").style.display = "none";
+        document.getElementById("English").style.display = "block";
+        window.localStorage.setItem("language", "English");
+        language = "English";
+
+    }
+
+};
