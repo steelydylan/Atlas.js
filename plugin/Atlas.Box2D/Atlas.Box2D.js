6918863a7cbe6727490fc5c5e9a97c88569a087b
XIpher-Desco <yukino.konata@gmail.com>
2014-07-05 18:20:25 +0900

    RBN-6 Atlas.jsでBox2D用のプラグインを作る

--- plugin/Atlas.Box2D/Atlas.Box2D.js
+++ plugin/Atlas.Box2D/Atlas.Box2D.js
@@ -19,14 +19,19 @@
         new b2Vec2(0, 10)    //gravity
         ,  true                 //allow sleep
     );
- 
-    Atlas.extendClass(Atlas.Thing,{
-        _enterFrame:function(){
-            /*if(this.body){
-                this.x = this.body.GetBody().GetPosition().x - this.width/2;
-                this.y = this.body.GetBody().GetPosition().y - this.height/2;
-                this.rot = this.body.GetBody().GetAngle();
-            }*/
+    var SCALE = 10;
+    
+    PhysBox = Atlas.createClass(Atlas.Shape.Box,{
+        initialize : function(color, width, height){
+            this.inherit(color, width, height);
+            this.setPosition(0,0);
+            this._applyPhisics();
+        },
+        _enterFrame : function(){
+            this.x = SCALE*(this.body.GetBody().GetPosition().x - this.width/(SCALE*2));
+            this.y = SCALE*(this.body.GetBody().GetPosition().y - this.height/(SCALE*2));
+            this.rot = this.body.GetBody().GetAngle();
+            if(this.body.sleep){this.color = "#FFFFFF";}
         },
         _applyPhisics:function(){
             bodyDef.type = b2Body.b2_dynamicBody;
@@ -40,45 +45,28 @@
             bodyDef.angle = this.rot;
             this.body = world.CreateBody(bodyDef).CreateFixture(fixDef);
         },
-        changeBody:function(){
-
-        }
-    });
-    PhysBox = Atlas.createClass(Atlas.Shape.Box,{
-        initialize : function(color, width, height,x,y){
-            this.inherit(color, width, height);
-            this.setPosition(x,y);
-            this._applyPhisics();
-        },
-        _enterFrame : function(){
-            this.x = SCALE*(this.body.GetBody().GetPosition().x - this.width/(SCALE*2));
-            this.y = SCALE*(this.body.GetBody().GetPosition().y - this.height/(SCALE*2));
-            this.rot = this.body.GetBody().GetAngle();
-        },
         setPosition: function (x, y) {
-            /*this.rot = this.body.GetBody().GetAngle();
-            world.DestroyBody(this.body.GetBody());
-            
-            this._applyPhisics();*/
-            //this.body.GetBody().
+            this.body.setPosition(x/SCALE,y/SCALE);
             this.x = x;
             this.y = y;
             return this;
         }
     });
-
+    
+    
+    
     Atlas.extendClass(Atlas.App,{
         createGround:function(x,y,width,height){
             bodyDef.type = b2Body.b2_staticBody;
-            bodyDef.position.x = x;
-            bodyDef.position.y = y;
+            bodyDef.position.x = x/SCALE;
+            bodyDef.position.y = y/SCALE;
             fixDef.shape = new b2PolygonShape;
-            fixDef.shape.SetAsBox(width, height);
+            fixDef.shape.SetAsBox(width/SCALE, height/SCALE);
             world.CreateBody(bodyDef).CreateFixture(fixDef);  
         },
         enterFrame:function(){
             world.Step(
-                1 / 30   //frame-rate
+                1 / 60   //frame-rate
                 ,  10       //velocity iterations
                 ,  10       //position iterations
             );
